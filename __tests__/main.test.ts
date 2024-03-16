3cbd3777fee80a838b5ba9582d1726b4afc75e41
Kevin Flathers <flatherskevin@gmail.com>
2024-03-16 09:37:27 -0400

    fix(build): Fixing builds (#98)
    * fix(build): Fixing builds
    
    * Adding messages

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,3 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import * as github from '@actions/github'
 import {expect, test} from '@jest/globals'
 import {
